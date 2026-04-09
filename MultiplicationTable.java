290ba7796e7eb0dd4cfef4297908049879662373
xiaochengou123 <xiaocheng4563@qq.com>
2026-04-09 15:00:53 +0800

    Update MultiplicationTable.java

--- MultiplicationTable.java
+++ MultiplicationTable.java
@@ -1,7 +1,7 @@
 public class MultiplicationTable {
     public static void main(String[] args) {
         System.out.println("九九乘法表：");
-        
+        //二个for循环，从 i 1~9  ,j 1~9 
         for (int i = 1; i <= 9; i++) {
             for (int j = 1; j <= i; j++) {
                 System.out.print(j + "×" + i + "=" + (i * j) + "\t");
